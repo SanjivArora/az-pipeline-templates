d854dfe4f5ce94a03054e84981cffacc19e8e6d2
Corrie Robbertse <corrie.robbertse@healthalliance.co.nz>
2023-11-13 16:52:30 +1300

    adding scripts

---
+++ scripts/azure_scripts.ps1
@@ -0,0 +1,4 @@
+reg import 'SCHANNEL hANZ Encryption Std v2.0 (2016+2019).reg'
+reg import 'vulnerability_fix_2019.reg'
+cscript c:\windows\system32\slmgr.vbs /ipk N69G4-B89J2-4G8F4-WWYCC-J464C
+cscript c:\windows\system32\slmgr.vbs /ato
